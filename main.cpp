ed067230f787151dd4ed093aab3d38e068ff884e
keithchasen <keithchasen89@gmail.com>
2023-12-09 21:36:19 +0100

    drawing circle and rectangle

--- main.cpp
+++ main.cpp
@@ -5,6 +5,12 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "GAME KEITH");
 
+    sf::CircleShape circle(30.f);
+    circle.setFillColor(sf::Color(200, 200, 0));
+
+    sf::RectangleShape rect(sf::Vector2f(120.f, 50.f));
+    
+
     // set frame rate limit
     // to run app at a given frame rate 
     // instead of monitor's frequency
@@ -18,6 +24,14 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+        window.clear(sf::Color::Black);
+
+        window.draw(circle);
+
+        window.draw(rect);
+
+        window.display();
     }
 
     return 0;
