1773d132811deef39f2d4d4c9bcbda068832fccd
keithchasen <keithchasen89@gmail.com>
2023-12-10 21:52:59 +0100

    basic wrapper classes for rect and circle

--- main.cpp
+++ main.cpp
@@ -1,5 +1,128 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 #include <SFML/Graphics.hpp>
+
+class CircleWrapper
+{
+    std::string name;
+
+    int m_x;
+    int m_y;
+
+    int m_sx;
+    int m_sy;
+
+    int m_R;
+    int m_G;
+    int m_B;
+
+    int m_r;
+
+public:
+
+};
+
+class RectWrapper
+{
+    std::string name;
+
+    int m_x;
+    int m_y;
+
+    int m_sx;
+    int m_sy;
+
+    int m_R;
+    int m_G;
+    int m_B;
+
+    int m_w;
+    int m_h;
+
+public:
+
+};
+
+class ShapeKeeper
+{
+    std::vector<CircleWrapper> circles;
+    std::vector<RectWrapper> rects;
+
+    int windowWidth;
+    int windowHeight;
+
+    std::string fontPath;
+    int fontSize;
+
+    int fontR;
+    int fontG;
+    int fontB;
+
+    void setFont(
+        const std::string& path, 
+        const int size, 
+        const int R, 
+        const int G, 
+        const int B
+    ) 
+    {
+        fontPath = path;
+        fontSize = size;
+
+        fontR = R;
+        fontG = G;
+        fontB = B;
+    }
+
+    void setWindowSettings(
+        const int W, 
+        const int H
+    )
+    {
+        windowWidth = W;
+        windowHeight = H;
+    }
+
+    void setRect(
+        std:: string name, 
+        int x, 
+        int y, 
+        int sx, 
+        int sy, 
+        int R, 
+        int G, 
+        int B, 
+        int w, 
+        int h
+    )
+    {
+
+    }
+
+    void setCircle(
+        std:: string name, 
+        int x, 
+        int y, 
+        int sx, 
+        int sy, 
+        int R, 
+        int G, 
+        int B, 
+        int r
+    )
+    {
+
+    }
+
+
+    void readShapesFromFile(const std::string& filename)
+    {
+        //input file stream
+        std::ifstream fileStream(filename);
+
+    }
+};
 
 int main()
 {
