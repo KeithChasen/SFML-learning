332c678446ecc0d57208fd5e78c70645f7c89956
keithchasen <keithchasen89@gmail.com>
2023-12-19 14:28:07 +0100

    entity manager, entity and components

---
+++ 2-geometry-fight/Entity.h
@@ -0,0 +1,32 @@
+#include <Components.h>
+#include <memory>
+#include <string>
+
+class Entity
+{
+    // only EntityManager can access private constructor of
+    // Entity
+    friend class EntityManager;
+
+    bool        m_active = true;
+    size_t      m_id     = 0;
+    std::string m_tag    = "default";
+
+    Entity(const size_t id, const std::string& tag);
+
+public:
+
+    //component pointers
+    std::shared_ptr<CTransform> CTransform;
+    std::shared_ptr<CShape> CShape;
+    std::shared_ptr<CCollision> CCollision;
+    std::shared_ptr<CInput> CInput;
+    std::shared_ptr<CScore> CScore;
+    std::shared_ptr<CLifespan> CLifespan;
+
+    bool isActive() const;
+    const std::string & tag() const;
+    const size_t id() const;
+    void destroy();
+};
+
